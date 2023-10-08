8afc7567af5df9c6658926f8c017f616f13382f1
Eric TK <ericatkusa@gmail.com>
2023-10-08 18:38:27 +0545

    Added Multiprocessing

--- newScrapper.py
+++ newScrapper.py
@@ -5,15 +5,17 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
 import os
-import re
+import time
+import multiprocessing
+time1=time.time()
 # Replace with the URL of the website with download buttons
 url = "https://www.keralanotes.com/2021/12/KTU-S3-CSE-Notes-Syllabus-Qbank-Textbook.html#"
-
 download_button_class = 'buttonDownload'  # Replace with the correct class name
 print("Script started")
 
 # Specify the directory where you want to save the downloaded files
- # Change this to your desired directory
+# Change this to your desired directory
+download_directory = "/home/coding/study/pdf"  # Change this to your desired directory
 
 try:
     # Send an HTTP GET request to the website
@@ -32,75 +34,78 @@ try:
         if "Notes" in link_url:
             note_links.append(link_url)
 
-    # Print the extracted note links
-    for link in note_links:
-        # print(link)
-        response = requests.get(link)
-        response.raise_for_status()  # Check for HTTP errors
-        soup = BeautifulSoup(response.text, 'html.parser')
-        subname=" ".join(link.split("/")[-1].split("-")[2:-2])
-        
-    # Find all download buttons or links
-        download_links = soup.find_all('a', class_=download_button_class)
+    # Initialize Google Drive API
+    credentials = service_account.Credentials.from_service_account_file('credits.json', scopes=['https://www.googleapis.com/auth/drive'])
+    drive_service = build('drive', 'v3', credentials=credentials)
 
-        # Initialize Google Drive API
-        credentials = service_account.Credentials.from_service_account_file('credits.json', scopes=['https://www.googleapis.com/auth/drive'])
-        drive_service = build('drive', 'v3', credentials=credentials)
+    def is_google_drive_link(link):
+        return 'drive.google.com' in link
 
-        def is_google_drive_link(link):
-            return 'drive.google.com' in link
+    # Batch download size (number of files to download concurrently)
+    batch_size = 4
 
-        # Filter and download files
-        for link in download_links:
-            # Extract the download URL
-            download_url = link.get('href')
+    # Split the note_links into batches
+    batches = [note_links[i:i+batch_size] for i in range(0, len(note_links), batch_size)]
 
-            # Decode the URL (if needed
-            download_url = urllib.parse.unquote(download_url)
-            # print("Processing", download_url)
+    # Function to download a batch of files
+    def download_batch(batch):
+        for link in batch:
+            response = requests.get(link)
+            response.raise_for_status()  # Check for HTTP errors
+            soup = BeautifulSoup(response.text, 'html.parser')
+            subname = " ".join(link.split("/")[-1].split("-")[2:-2])
 
-            if is_google_drive_link(download_url):
-                # Extract the file ID from the download URL
-                file_id = download_url.split('/')[-2]
-                # print("File ID is ",file_id)
-                # Construct the direct download link
-                direct_download_link = f"https://drive.google.com/uc?export=download&id={file_id}"
-                # print("Downliad is ",direct_download_link )
-                file = drive_service.files().get(fileId=file_id).execute()
-                filename = file['name']
-                try:
-                    os.mkdir("/home/coding/study/pdf/"+subname)
-                except:
-                    print()
-                # Get the file name from the link
-                # filename = download_url.split('/')[-2]
-                # content_disposition = response.headers.get("Content-Disposition")
-                # if content_disposition:
-                #     match = re.search('filename="(.+)"', content_disposition)
-                #     if match:
-                #         filename = match.group(1)
-                #     else:
-                #         filename = "downloaded_file.bin"  # Default filename if not found in the header
-                # else:
-                #     filename = "downloaded_file.bin" 
-                download_directory = "/home/coding/study/pdf/"+subname 
-                # print(download_directory)
-                c=1
+            # Find all download buttons or links
+            download_links = soup.find_all('a', class_=download_button_class)
 
-                local_file_path = os.path.join(download_directory, filename)  # Specify local file path
-                
-                # print("Downloading", filename)
+            # Filter and download files
+            for link in download_links:
+                # Extract the download URL
+                download_url = link.get('href')
 
-                # Download the file and save it locally
-                with open(local_file_path, 'wb') as f:
-                    response = requests.get(direct_download_link)
-                    response.raise_for_status()
-                    f.write(response.content)
-                    c+=1
+                # Decode the URL (if needed)
+                download_url = urllib.parse.unquote(download_url)
 
-                print(f"Downloaded: {filename} to {local_file_path}")
+                if is_google_drive_link(download_url):
+                    # Extract the file ID from the download URL
+                    file_id = download_url.split('/')[-2]
+
+                    # Get the file metadata to obtain the filename
+                    file = drive_service.files().get(fileId=file_id).execute()
+                    filename = file['name']
+                    # print(filename)
+
+                    # Create a subdirectory for the batch
+                    try:
+                        os.mkdir(os.path.join(download_directory, subname))
+                    except FileExistsError:
+                        pass
+                    time.sleep(10)
+                    # Specify the local file path
+                    local_file_path = os.path.join(download_directory, subname, " ".join(filename.split()[0:3]) + ".pdf")
+
+                    # Download the file and save it locally
+# Download the file and save it locally
+                    with open(local_file_path, 'wb') as f:
+                        response = requests.get(download_url, stream=True)
+                        response.raise_for_status()
+                        for chunk in response.iter_content(chunk_size=8192):
+                            f.write(chunk)
+
+
+                    print(f"Downloaded: {filename} to {local_file_path}")
+
+    # Create a pool of worker processes for concurrent batch downloads
+    pool = multiprocessing.Pool(processes=len(batches))
+
+    # Use the pool to download batches concurrently
+    pool.map(download_batch, batches)
+
+    # Close the pool of worker processes
+    pool.close()
+    pool.join()
 
     print("Script completed")
-
+    print("Program took",time.time()-time1," Seconds")
 except Exception as e:
     print("Error:", str(e))
